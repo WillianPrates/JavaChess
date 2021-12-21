eb0fdcb04368cca42c6992e27b822d1c82362981
WillianPrates <71357338+WillianPrates@users.noreply.github.com>
2021-12-21 11:48:29 -0300

    Master (#2)
    * classe position
    
    * commit comico

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Board;
+import tabuleiro.Position;
+
 public class Program {
 
     public static void main(String[] args){
 
-        System.out.println("Hello Word");
+        Board board = new Board(8,8);
+
     }
 }
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,11 @@
+package chess;
+
+import tabuleiro.Board;
+import tabuleiro.Piece;
+
+public class ChessPiece extends Piece {
+
+    public ChessPiece(Board board) {
+        super(board);
+    }
+}
